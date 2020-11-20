52e52e8e360abf5d008f2a20499406d9c10eb02d
고규훈 <kh.ko@airpremia.com>
2020-11-20 18:55:06 +0900

    Add font, normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Axios from 'axios';
 
 function App() {
@@ -16,10 +14,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Edit and save to reload.</p>
+        <p>무궁화 꽃이 피었습니다.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
