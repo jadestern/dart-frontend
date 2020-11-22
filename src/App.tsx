492306a7177dfc92e968005c89977d1872142650
고규훈 <kh.ko@airpremia.com>
2020-11-22 16:17:58 +0900

    데스크탑 환경 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { SearchBar, List } from './components';
 
 function App() {
   return (
-    <div className="App px-6 py-8">
+    <div className="App px-6 py-8 max-w-md mx-auto">
       <SearchBar />
       <List />
     </div>
