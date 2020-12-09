2e2cef1ff8fdad9933d110c6e07004160305bc7a
고규훈 <kh.ko@airpremia.com>
2020-12-09 09:32:41 +0900

    delete react-ga

--- src/ga.ts
+++
@@ -1,3 +0,0 @@
-import ReactGA from 'react-ga';
-ReactGA.initialize(process.env.REACT_APP_GA || '');
-ReactGA.pageview(window.location.pathname + window.location.search);
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tailwindcss/tailwind.css';
-import './ga';
 import App from './App';
 
 ReactDOM.render(
